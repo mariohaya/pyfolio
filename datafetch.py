deb74983415f01dd96b72ed0fb483a566f4bfd52
mariohaya <marioh.922@hotmail.com>
2026-05-26 16:12:48 -0500

    cov matrix

--- datafetch.py
+++ datafetch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+t!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # master
 """
@@ -10,20 +10,19 @@ Created on Mon Mar  9 11:45:41 2026
 from datetime import datetime
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
-##### Initial input
+##### Initial inputs
 print("Enter ticker(s)")
 name = input("Ticker: ")
-
-# tickers
+    # tickers
 ticker = name.strip().upper()
-
-# date range
-##start_date = "2025-12-01"
-##end_date= "2026-01-10"
+    # date range
+        ##start_date = "2025-12-01"
+        ##end_date= "2026-01-10"
 end_date = datetime.today()
 start_date = end_date.replace(year=end_date.year - 5)
-# string format
+        # string format
 start_date = start_date.strftime("%Y-%m-%d")
 end_date = end_date.strftime("%Y-%m-%d")
 
@@ -39,5 +38,24 @@ df = yf.download(
 prices = df["Adj Close"]
 dr = prices.pct_change().dropna()
 
-print(dr.head())
+# avg annualized rets
+mean_returns = dr.mean() * 252
+num_assets = len(mean_returns)
+
+print("\n INDIVIDUAL ASSET (annualized) EXPECTED RETURNS:")
+print(mean_returns)
+
+# covariance matrix
+matrix = dr.cov() * 252
+print("\n ASSET COVARIANCE MATRIX:")
+print(matrix)
+
+print("\n NO. OF ASSETS IN PORTFOLIO:")
+print(num_assets)
+
+#### Constraints
+# 1. Weight sum = 1
+# 2. No short selling, i.e. w > 0
+# 3. No leveraged assets, i.e. w > 1
+
 
