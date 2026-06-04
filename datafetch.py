b7f433c6d45ffcc4b6215f1eb98760199b9270a9
mariohaya <marioh.922@hotmail.com>
2026-06-04 12:00:21 -0500

    fixed weights

--- datafetch.py
+++ datafetch.py
@@ -1,4 +1,4 @@
-t!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # master
 """
@@ -6,39 +6,34 @@ Created on Mon Mar  9 11:45:41 2026
 
 @author: mario
 """
-##### libs
+## libraries
 from datetime import datetime
 import yfinance as yf
 import pandas as pd
 import numpy as np
 
-##### Initial inputs
+## Initial inputs
 print("Enter ticker(s)")
 name = input("Ticker: ")
-    # tickers
-ticker = name.strip().upper()
-    # date range
-        ##start_date = "2025-12-01"
-        ##end_date= "2026-01-10"
+ticker = name.strip().upper() # tickers
 end_date = datetime.today()
-start_date = end_date.replace(year=end_date.year - 5)
-        # string format
+start_date = end_date.replace(year=end_date.year - 5) # 5yr window
 start_date = start_date.strftime("%Y-%m-%d")
 end_date = end_date.strftime("%Y-%m-%d")
 
 df = yf.download(
-    ticker,
-    start=start_date,
-    end=end_date,
-    auto_adjust=False,
+    ticker, #t
+    start=start_date, #s
+    end=end_date, #e
+    auto_adjust=False, 
     progress=False
     )
 
-#daily ret
+#daily returns
 prices = df["Adj Close"]
 dr = prices.pct_change().dropna()
 
-# avg annualized rets
+# avg annualized returns
 mean_returns = dr.mean() * 252
 num_assets = len(mean_returns)
 
@@ -57,5 +52,11 @@ print(num_assets)
 # 1. Weight sum = 1
 # 2. No short selling, i.e. w > 0
 # 3. No leveraged assets, i.e. w > 1
+
+## weights
+fixed_weights = np.array([1 / num_assets] * num_assets)
+fixed_portfolio_return = np.dot(fixed_weights, mean_returns)
+print("\n Expected portfolio returns (fixed weights)"
+print(fixed_portfolio_return)
 
 
