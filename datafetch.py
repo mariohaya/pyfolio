1d65eaf0be2e9a22571bc35eac6286a924162108
mariohaya <marioh.922@hotmail.com>
2026-06-04 13:04:28 -0500

    sharpe

--- datafetch.py
+++ datafetch.py
@@ -8,6 +8,7 @@ Created on Mon Mar  9 11:45:41 2026
 """
 ## libraries
 from datetime import datetime
+from scipy.optimize import minimize #solver
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -20,6 +21,8 @@ end_date = datetime.today()
 start_date = end_date.replace(year=end_date.year - 5) # 5yr window
 start_date = start_date.strftime("%Y-%m-%d")
 end_date = end_date.strftime("%Y-%m-%d")
+
+rf_rate = input("\nRisk-free rate, e.g. (0.03):")
 
 df = yf.download(
     ticker, #t
@@ -51,12 +54,23 @@ print(num_assets)
 #### Constraints
 # 1. Weight sum = 1
 # 2. No short selling, i.e. w > 0
-# 3. No leveraged assets, i.e. w > 1
+# 3. No leveraged assets, i.e. w < 1
 
-## weights
+## fixed weights
 fixed_weights = np.array([1 / num_assets] * num_assets)
-fixed_portfolio_return = np.dot(fixed_weights, mean_returns)
-print("\n Expected portfolio returns (fixed weights)")
-print(fixed_portfolio_return)
+fixed_p_return = np.dot(fixed_weights, mean_returns)
+fpr_percent = fixed_p_return 
+fpr_percent = format(fpr_percent, ".2%")
+f_p_sd = np.sqrt(np.dot(fixed_weights.T, np.dot(matrix,fixed_weights)))
+f_p_sd_percent = format(f_p_sd, ".2%")
+print("\n The expected yearly return of your portfolio is", fpr_percent, "volatility", f_p_sd_percent)
+
+## optimized weights
+# sharpe = (p_return - rf_rate) / p_volatility
+
+def p_return(weights):
+    return np.dot(weights, mean_returns)
+
+
 
 
